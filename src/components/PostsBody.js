c2ae073f9291691e182d8542d08e7adef1afe914
Satyam Kumar Rai <95238430+Satyamrai09@users.noreply.github.com>
2022-07-21 23:50:14 +0530

    Update PostsBody.js

--- src/components/PostsBody.js
+++ src/components/PostsBody.js
@@ -1,28 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container } from 'reactstrap'
-import axios from 'axios'
+// import axios from 'axios'
 import PostCard from './PostCard'
 
 export default function PostsBody() {
 
     // https://hn.algolia.com/api/v1/search?query={keyword}
 
-    const baseURL = ''
+    const [items, setItems] = useState([]);
+    const [query, setQuery] = useState("");
+   
+    const [searchParam] = useState(["title", "author", "num_comments"]);
+    useEffect(() => {
+        // LoadData();
+        getData();
+    }, []);
+
+    async function getData() {
+        const baseURL = 'https://hn.algolia.com/api/v1/search?query={keyword}'
+        const result = await fetch(baseURL);
+        const getResult = await result.json();
+        setItems(getResult);
+        console.log(getResult)
+    }
 
     return (
         <Container>
             <form className='form-group mt-5 d-flex'>
-                <input type="text" className='form-control me-2' />
+                <input type="text" className='form-control me-2' value={query} onChange={(e) => setQuery(e.target.value)} />
             </form>
-            
+
             <div className='container bg-white results-container p-4 my-4'>
 
-                    <PostCard title="Title 1 Goes here" num_comments="101"
-                        author="Author 1" url="#" />
-                    <PostCard title="Title 2 Goes here" num_comments="201"
-                        author="Author 2" url="#" />
-                    <PostCard title="Title 1 Goes here" num_comments="301"
-                        author="Author 3" url="#" />
+
+                {items.hits.map(item => (
+                    <li key={item.created_at}>
+                        <PostCard
+                            title={item.title}
+                            num_comments={item.num_comments}
+                            author={item.author}
+                            url="baseURL"
+                        />
+                    </li>
+                ))}
+
+
 
             </div>
         </Container>
