18fc287cada82b52935f5125f7330cbde6ae60f3
Satyam Kumar Rai <95238430+Satyamrai09@users.noreply.github.com>
2022-07-22 03:15:12 +0530

    Update PostsBody.js

--- src/components/PostsBody.js
+++ src/components/PostsBody.js
@@ -1,30 +1,42 @@
-import React from 'react'
-import { Container } from 'reactstrap'
-import axios from 'axios'
-import PostCard from './PostCard'
+import React, { useState, useEffect } from 'react';
+import { Container } from 'reactstrap';
+import axios from 'axios';
+import PostCard from './PostCard';
+
+
 
 export default function PostsBody() {
+    const [query, setQuery] = useState([]);
+    const [search, setSearch] = useState("");
 
-    // https://hn.algolia.com/api/v1/search?query={keyword}
+    useEffect(() => {
+        axios
+          .get("https://hn.algolia.com/api/v1/search?query={react}")
+          .then((res) => {
+            console.log("response data", res.data.getAllprop);
+            setQuery(res.data.hits);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }, []);
+  
+return (
+    <Container>
+        <form className='form-group mt-5 d-flex'>
+            <input type="text" className='form-control me-2' onClick={(event)=> {setSearch(event.target.value);}}  />
+        </form>
 
-    const baseURL = ''
+        <div className="container bg-white results-container p-4 my-4">
+        {query.filter((val)=>{
+         if(search===""){
+            return val
+         }else if(val.title.toLowerCase().includes(search.toLowerCase())){
+            return val
+         }
+        }).map((prop, key) => (<PostCard title={prop.title} author={prop.author} num_comments={prop.num_comments}></PostCard>))}
+        </div>
+    </Container>
+);
 
-    return (
-        <Container>
-            <form className='form-group mt-5 d-flex'>
-                <input type="text" className='form-control me-2' />
-            </form>
-            
-            <div className='container bg-white results-container p-4 my-4'>
-
-                    <PostCard title="Title 1 Goes here" num_comments="101"
-                        author="Author 1" url="#" />
-                    <PostCard title="Title 2 Goes here" num_comments="201"
-                        author="Author 2" url="#" />
-                    <PostCard title="Title 1 Goes here" num_comments="301"
-                        author="Author 3" url="#" />
-
-            </div>
-        </Container>
-    )
 }
